03c4b7c3ab70b8fac42dc30230cd6b3a9d5ea4c1
Alan Jorge Garcia Apaza <agarciaa@unsa.edu.pe>
2025-07-13 20:50:27 -0500

    iniciando pruebas de rendimiento

---
+++ carga/RF1.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 50,          // 10 usuarios virtuales
+  duration: '30s',  // durante 30 segundos
+};
+
+export default function () {
+  let res = http.get('https://8-0-0-dot-teammates-grasshoppers-testing.uw.r.appspot.com/web/front/request');
+
+  check(res, {
+    'status es 200': (r) => r.status === 200,
+    'carga no vacía': (r) => r.body && r.body.length > 500,
+  });
+
+  sleep(1); // espera antes de repetir
+}
